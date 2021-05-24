58057ee501c93210b34dada3fcc1c2ecfa5091f6
samelabrito <samelacbrito08@gmail.com>
2021-05-24 18:15:00 -0300

    Samela modificações

--- Esquadritec/resources/views/cliente/new_cliente.blade.php
+++ Esquadritec/resources/views/cliente/new_cliente.blade.php
@@ -7,20 +7,54 @@
         <meta name="description" content="Tela de novo usuário">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/app.css">
+
+        <style>
+            .center{
+                top: 100px;
+                text-align: center;
+            }
+            .full-width{
+                width: 100%;
+                height: 100%;
+            }
+            .input{
+                position:relative;
+                padding-top:10px;
+                margin-top:10px;
+            }
+            .card{
+                padding: 20px;
+                width: 300px;
+            }
+        </style>
+
     </head>
     <body>
-        <form method="POST" action="{{ route('user_store') }}">
-            @csrf
-            <input type="text" name="name" placeholder="NOME">
-            <input type="text" name="cpf" placeholder="CPF">
-            <input type="text" name="cnpj" placeholder="CNPJ">
-            <input type="text" name="email" placeholder="EMAIL">
-            <input type="text" name="password" placeholder="SENHA">
-            <input type="text" name="password_2" placeholder="CONFIRMAR SENHA">
-            <label for="admin">ADMIN</label>
-            <input id="admin" type="checkbox" name="admin" value="1">
-            <input type="submit" value="ENVIAR">
-        </form>
+
+        <div class="container card center" style="">
+        <h2 class="container card center">NOVO CLIENTE</h2>
+            <div class="row justify-content-md-center">
+                <div class="col-md-auto">
+                    <form method="POST" action="{{ route('user_store') }}">
+                        @csrf
+                        <input type="text" name="name" placeholder="NOME:">
+                        <input type="text" name="cpf" placeholder="CPF:">
+                        <input type="text" name="cnpj" placeholder="CNPJ:">
+                        <input type="text" name="email" placeholder="EMAIL:">
+                        <input type="text" name="tel" placeholder="TELEFONE:">
+                        <h5 class="container card center">ENDEREÇO</h5>
+                        <input type="text" name="city" placeholder="CIDADE:">
+                        <input type="text" name="cpf" placeholder="CPF:">
+                        <input type="text" name="cnpj" placeholder="CNPJ:">
+                        <input type="text" name="email" placeholder="EMAIL:">
+                        <input type="text" name="tel" placeholder="TELEFONE:">
+                        <button class="btn btn-lg btn-primary btn-block input" type="submit">Cancelar</button>
+                        <button class="btn btn-lg btn-primary btn-block input" type="submit">Confirmar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
         <script src="" async defer></script>
     </body>
 </html>
--- Esquadritec/routes/web.php
+++ Esquadritec/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\User;
 use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\materiais\Material;
-
+use App\Htto\Controllers\cliente\new_cliente;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -22,6 +22,12 @@ Route::get('/dashboard/user/{id}', [User::class, 'show'])->name('user_show')->mi
 
 Route::get('/dashboard/new_material', [Material::class, 'index'])->name('new_material')->middleware('Validate');
 Route::post('/dashboard/new_material/new_material_do', [Material::class, 'create'])->name('new_material_do')->middleware('Validate');
+
+
+Route::get('/test', function () {
+    return view('cliente/new_cliente');
+});
+
 
 // POST
 Route::post('user_in', [User::class, 'store'])->name('user_store')->middleware('Validate');
