a37b7665fa2997ba7584901d7c2b7f5011e0fbb2
samelabrito <samelacbrito08@gmail.com>
2021-05-24 12:25:55 -0300

    modificações tela de login

--- Esquadritec/resources/views/auth/login.blade.php
+++ Esquadritec/resources/views/auth/login.blade.php
@@ -9,7 +9,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('site/style.css')}}">
 </head>
-
+<style>
+    .center{
+        text-align: center;
+    }
+    .center-card{
+        position: absolute;
+        text-align: center;
+    }
+    .full-width{
+        width: 100%;
+    }
+</style>
 <body>
     @if($errors->all())
     @foreach($errors->all() as $error)
@@ -17,27 +28,23 @@
     @endforeach
     @endif
 
-    <div id="login">
-        <h3 class="text-center text-white pt-5">Login form</h3>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-4 col-md-offset-4">
-                    <h1 class="text-center login-title"></h1>
-                    <div class="account-wall">
-                        <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                            alt="">
-                        <form class="form-signin">
-                            @csrf
-                        <input type="text" class="form-control" placeholder="Usuario" required autofocus>
-                        <input type="password" class="form-control" placeholder="senha" required>
-                        <button class="btn btn-lg btn-primary btn-block" type="submit">
-                            Entrar</button>
-                        </form>
-                    </div>
-                </div>    
+    <div class="center-card">
+        <div  class="card p-2 center gy-5" style="width: 400px;">
+            <h3 class="text-center text-white pt-5">Login form</h3>
+            <div class="p-3 center">
+                <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+                alt="">
             </div>
-        </div>
+            <form class="form-signin full-width">
+                @csrf
+            <input type="text" class="form-control" placeholder="Usuario" required autofocus>
+            <input type="password" class="form-control" placeholder="senha" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">
+                Entrar</button>
+            </form>
+        </div>    
     </div>
+
 
     <script src="{{ asset('site/jquery.js') }}" async defer></script>
     <script src="{{ asset('site/bootstrap.js') }}" async defer></script>
