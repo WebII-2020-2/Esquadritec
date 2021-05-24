5e93c714aea87f7da6035c25c7ba408f3df5f208
samelabrito <samelacbrito08@gmail.com>
2021-05-24 11:16:21 -0300

    Nova tela de login

--- Esquadritec/resources/views/auth/login.blade.php
+++ Esquadritec/resources/views/auth/login.blade.php
@@ -16,12 +16,28 @@
     <p>{{$error}}</p>
     @endforeach
     @endif
-    <form action="{{route('login/do')}}" method="POST">
-        @csrf
-        <input type="text" name="email" placeholder="E-mail" required>
-        <input type="password" name="password" placeholder="Senha" required>
-        <button type="submit" class="btn btn-success">ENTRAR</button>
-    </form>
+
+    <div id="login">
+        <h3 class="text-center text-white pt-5">Login form</h3>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-4 col-md-offset-4">
+                    <h1 class="text-center login-title"></h1>
+                    <div class="account-wall">
+                        <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+                            alt="">
+                        <form class="form-signin">
+                            @csrf
+                        <input type="text" class="form-control" placeholder="Usuario" required autofocus>
+                        <input type="password" class="form-control" placeholder="senha" required>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit">
+                            Entrar</button>
+                        </form>
+                    </div>
+                </div>    
+            </div>
+        </div>
+    </div>
 
     <script src="{{ asset('site/jquery.js') }}" async defer></script>
     <script src="{{ asset('site/bootstrap.js') }}" async defer></script>
