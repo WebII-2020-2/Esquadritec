9eebe263525288b101ef6e6ffc20bbbb2b07b5a9
djalma neto <djalma7743@gmail.com>
2021-05-24 12:57:58 -0300

    alteração pagina login

--- Esquadritec/resources/views/auth/login.blade.php
+++ Esquadritec/resources/views/auth/login.blade.php
@@ -11,14 +11,21 @@
 </head>
 <style>
     .center{
-        text-align: center;
-    }
-    .center-card{
-        position: absolute;
+        top: 100px;
         text-align: center;
     }
     .full-width{
         width: 100%;
+        height: 100%;
+    }
+    .input{
+        position:relative;
+        padding-top:10px;
+        margin-top:10px;
+    }
+    .card{
+        padding: 20px;
+        width: 300px;
     }
 </style>
 <body>
@@ -28,21 +35,23 @@
     @endforeach
     @endif
 
-    <div class="center-card">
-        <div  class="card p-2 center gy-5" style="width: 400px;">
-            <h3 class="text-center text-white pt-5">Login form</h3>
-            <div class="p-3 center">
-                <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                alt="">
+    <div class="container card center" style="">
+        <div class="row justify-content-md-center">
+            <div class="col-md-auto">
+                <div class="p-3 center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>
+                </div>
+                <form class="g-5">
+                    @csrf
+                    <input type="text" class="input full-width" placeholder="Usuario" required autofocus>
+                    <input type="password" class="input full-width" placeholder="senha" required>
+                    <button class="btn btn-lg btn-primary btn-block input" type="submit">Entrar</button>
+                </form>
             </div>
-            <form class="form-signin full-width">
-                @csrf
-            <input type="text" class="form-control" placeholder="Usuario" required autofocus>
-            <input type="password" class="form-control" placeholder="senha" required>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">
-                Entrar</button>
-            </form>
-        </div>    
+        </div>
     </div>
 
 
