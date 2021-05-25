fdad442eb97a91ad2e9ba7dfcdcabece39427007
samelabrito <samelacbrito08@gmail.com>
2021-05-24 23:52:14 -0300

    samela - tela de novo cliente

--- Esquadritec/resources/views/cliente/new_cliente.blade.php
+++ Esquadritec/resources/views/cliente/new_cliente.blade.php
@@ -6,53 +6,111 @@
         <title>Novo Usuário</title>
         <meta name="description" content="Tela de novo usuário">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" href="{{asset('site/style.css')}}">
 
         <style>
+            #body{
+                color: blue;
+            }
+
             .center{
                 top: 100px;
                 text-align: center;
             }
+
             .full-width{
                 width: 100%;
-                height: 100%;
             }
+
             .input{
                 position:relative;
                 padding-top:10px;
                 margin-top:10px;
             }
+
             .card{
                 padding: 20px;
-                width: 300px;
+                width: 1008pxpx;
+                background: #318A90;
+                border-radius: 28px;
+            }
+
+            .btn{
+                height: 30px;
+                text-align: center;
             }
         </style>
 
     </head>
     <body>
 
+        <h2>NOVO CLIENTE</h2>
         <div class="container card center" style="">
-        <h2 class="container card center">NOVO CLIENTE</h2>
-            <div class="row justify-content-md-center">
-                <div class="col-md-auto">
-                    <form method="POST" action="{{ route('user_store') }}">
-                        @csrf
-                        <input type="text" name="name" placeholder="NOME:">
-                        <input type="text" name="cpf" placeholder="CPF:">
-                        <input type="text" name="cnpj" placeholder="CNPJ:">
-                        <input type="text" name="email" placeholder="EMAIL:">
-                        <input type="text" name="tel" placeholder="TELEFONE:">
-                        <h5 class="container card center">ENDEREÇO</h5>
-                        <input type="text" name="city" placeholder="CIDADE:">
-                        <input type="text" name="cpf" placeholder="CPF:">
-                        <input type="text" name="cnpj" placeholder="CNPJ:">
-                        <input type="text" name="email" placeholder="EMAIL:">
-                        <input type="text" name="tel" placeholder="TELEFONE:">
-                        <button class="btn btn-lg btn-primary btn-block input" type="submit">Cancelar</button>
-                        <button class="btn btn-lg btn-primary btn-block input" type="submit">Confirmar</button>
-                    </form>
+
+                <div class="row justify-content-md-center">
+                    <div class="col-md-auto">
+                        
+                        <form method="POST" action="{{ route('user_store') }}">
+                            @csrf
+                            <div class="rounded-pill form-group mb-2">
+                                <label for="name">NOME:</label>
+                                <input type="text" name="name">
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="cpf">CPF:</label>
+                                <input type="text" name="cpf">
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="cnpj">CNPJ:</label>
+                                <input type="text" name="cnpj"> 
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="email">EMAIL:</label>
+                                <input type="text" name="email">    
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="tel">TELEFONE:</label>
+                                <input type="text" name="tel">    
+                            </div>
+                
+                            <h5 class="mt-3">ENDEREÇO</h5>
+                            
+                            <div class="form-group mb-2">
+                                <label for="city">CIDADE:</label>
+                                <input type="text" name="city">   
+                            </div>
+                            
+                            <div class="form-group mb-2">
+                                <label for="road">RUA:</label>
+                                <input type="text" name="road">   
+                            </div>
+                            
+                            <div class="form-group mb-2">
+                                <label for="district">BAIRRO:</label>
+                                <input type="text" name="district">   
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="number">NÚMERO:</label>
+                                <input type="text" name="number">   
+                            </div>
+                            
+                            <div class="form-group mb-2">
+                                <label for="observation">OBSERVAÇÕES:</label>
+                                 <input type="text" name="observation">   
+                            </div>
+
+                            <div class="full-width p-2" >
+                                <button class="rounded-pill btn btn-md btn-danger" type="submit">Cancelar</button>
+                                <button class="rounded-pill btn btn-md btn-success" type="submit">Confirmar</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
         </div>
 
         <script src="" async defer></script>
