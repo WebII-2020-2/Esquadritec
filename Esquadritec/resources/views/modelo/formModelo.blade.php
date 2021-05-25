7a27d4e3254db8db268c7eedceb27dea99a0b402
EliveltonCotrim <elivelton.gbi@gmail.com>
2021-05-25 00:46:36 -0300

    tela formModelo

--- Esquadritec/resources/views/modelo/formModelo.blade.php
+++ Esquadritec/resources/views/modelo/formModelo.blade.php
@@ -6,24 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Tela novo modelo">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{asset('site/style.css')}}">
+
 
     <title>Novo modelo</title>
 </head>
+<style>
+    .card {
+        background-color: #318A90;
+        border-radius: 28px;
+        height: 255px;
+    }
+
+    label {
+        color: white;
+    }
+</style>
 
 <body>
-    <h1>Novo modelo</h1>
+    <div class="py-4" style="text-align: center;">
+        <h2 class="text-main font-monospace ">NOVO MODELO</h2>
+    </div>
+    <div class="container card center" style="width: 500px;">
 
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+        <div class="row justify-content-md-center pt-4" style="text-align: center;">
+            <div class="col-md-auto">
+                <form class="g-5">
+                    @csrf
+                    <div class="row p-3 center">
+                        <label for="modelo" class="pr-2" style="color: white">Modelo:</label>
+                        <input type="text" class="form-control" id="modelo" style="width: 200px;" placeholder="">
+                    </div>
+                    <div class="pt-4 g-4">
+                        <button class="rounded-pill btn btn-md btn-green" type="submit">Cadastrar</button>
+
+                        <button class="rounded-pill btn btn-md btn-cancelar" type="submit">Cancelar</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
+
 
 
 </body>
