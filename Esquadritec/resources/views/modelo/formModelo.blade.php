d0bf6632220f8e35a6a2eebb7009efcecc5bcba6
EliveltonCotrim <elivelton.gbi@gmail.com>
2021-05-31 18:12:35 -0300

    .

--- Esquadritec/resources/views/modelo/formModelo.blade.php
+++ Esquadritec/resources/views/modelo/formModelo.blade.php
@@ -48,9 +48,69 @@
         line-height: 49px;
 
     }
+
+    .navbar.custom {
+        width: 50%;
+    }
+
+    .right-addon input {
+        padding-right: 30px;
+    }
+
+    .inner-addon {
+        position: relative;
+    }
 </style>
 
 <body>
+
+    <nav class="navbar navbar-light bg-main">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a class="navbar-brand text-white px-2 m-0" href="#">ESQUADRITEC</a>
+
+            </div>
+            <div class="inner-addon right-addon">
+                <i class="glyphicon glyphicon-search"></i>
+                <input type="text" class="form-control" placeholder="Search" />
+            </div>
+
+            <a class="navbar-brand text-white" href="#">User</a>
+
+
+    </nav>
+
+    <div class="collapse navbar-collapse  bg-main" id="navbarTogglerDemo03">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active text-white" href="#">Funcionário</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active text-white" href="#">Cliente</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active text-white" href="#">Orçamento</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active text-white" href="#">Material</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active text-white" href="#">Modelo</a>
+            </li>
+        </ul>
+    </div>
+
+    </div>
+
+    </nav>
+
+
 
     <div class="py-4 center" style="min-height: 200px;">
         <h2 class="text-main font-monospace ">NOVO MODELO</h2>
@@ -70,5 +130,8 @@
         </div>
     </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+</script>
 
 </html>
