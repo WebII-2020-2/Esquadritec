b03c2b09cca8b48d95f849e2dd986ea05267128e
Elivelton <elivelton.gbi@gmail.com>
2021-07-13 08:52:00 -0300

    view nova linha finalizada

--- Esquadritec/resources/views/linha/formLinha.blade.php
+++ Esquadritec/resources/views/linha/formLinha.blade.php
@@ -6,70 +6,96 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Tela nova linha">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('site/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('site/style.css') }}">
 
 
     <title>Nova Linha</title>
 </head>
 <style>
-    .card {
-        background-color: #318A90;
-        border-radius: 28px;
-        height: 255px;
+    .container {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 
-    .center {
-        text-align: center;
+    .card_form {
+        width: 400px;
+        height: 300px;
+        position: relative;
+        display: flex;
+        height: 200px;
+        background-color: #295C8A;
+        border-radius: 10px;
     }
 
-    .center-div {
-        margin: auto;
-        width: 50%;
-        padding: 10px;
+    .card_form form {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 0px
     }
 
-    .form-control {
-        width: 65%;
+    .div_input {
+        width: 100%;
     }
 
-    .bottom-div {
-        position: absolute;
-        bottom: 20%;
-        left: 25%;
+    .div_input {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+
+    }
+
+    
+    .div_botao{
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        margin-top: 50px;
     }
 
     label {
         color: white;
+        margin-inline-end: 8px;
     }
 
     h2 {
+        text-align: center;
         font-size: 30px;
         font-weight: 700;
         line-height: 49px;
 
     }
+
 </style>
 
 <body>
-    <x-layout/>
-    <div class="py-4 center" style="min-height: 200px;">
+    <x-layout />
+    <div class="container" style="min-height: 200px;">
         <h2 class="text-main font-monospace ">NOVA LINHA</h2>
 
-        <div class="card-generico center-div" style="width: 500px;min-height: 100%;">
-            <form class="" style="height: 100%;" action="{{route('new_linha_do')}}" method="POST">
+        <div class="card_form" style="">
+            <form class="" style="" action="{{ route('new_linha_do') }}" method="POST">
                 @csrf
-                <div class="row p-3 center-div pt-5" style="width: 300px;">
-                    <label for="linha" class="pr-2 col-form-label   " style="color: white">LINHA:</label>
-                    <input id="linha" type="text" name="linha" class="form-control" placeholder="">
+                <div class="div_input" style="">
+                    <label for="linha" class="col-form-label" style="color: white;">LINHA:</label>
+                    <input id="linha" type="text" name="linha" class="form-control" style="width: 35%;"
+                        placeholder="">
                 </div>
-                <div class="center-div bottom-div w-auto">
-                    <button class="rounded-pill btn btn-md btn-cancelar mx-4" onClick="goBack()" type="reset">Cancelar</button>
+                <div class="div_botao">
+                    <button class="rounded-pill btn btn-md btn-cancelar" onClick="goBack()"
+                        type="reset">Cancelar</button>
                     <button class="rounded-pill btn btn-md btn-green" type="submit">Adicionar</button>
                 </div>
             </form>
         </div>
     </div>
-    <x-footer/>
+    <x-footer />
 </body>
 
 </html>
