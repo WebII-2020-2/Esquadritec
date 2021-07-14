7921c5c4ac087dbd84cb35df8a723408fe17ea6a
Elivelton <elivelton.gbi@gmail.com>
2021-07-14 19:13:59 -0300

    finalização das tabelas da seção material

--- Esquadritec/resources/views/materiais/showMaterial.blade.php
+++ Esquadritec/resources/views/materiais/showMaterial.blade.php
@@ -4,23 +4,44 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{$material->nome}}</title>
+    <title>{{ $material->nome }}</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{asset('site/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('site/style.css') }}">
     <script>
         // corrigir essa funçaõ para passar valor pela url
     </script>
 </head>
+<style>
+    .tabela {
+        border: rgba(204, 204, 204, 0.952) solid 1px;
+        width: 100%;
+
+        text-align: center;
+    }
+    .cabecalho th {
+        border: rgba(204, 204, 204, 0.952) solid 1px;
+    }
+    th {
+        color: white;
+    }
+    .table_rows {
+        color: black;
+        border: rgba(204, 204, 204, 0.952) solid 1px;
+
+    }
+
+
+</style>
 
 <body>
     <x-layout />
-    <h2>{{$material->nome}}</h2>
+    <h2 class="text-black font-monospace" style="font-size: 25px;">{{ $material->nome }}</h2>
 
-    <div class="card-generico table">
-        <table class="table table-sm center">
-            <thead>
-                <tr>
+    <div class="card1" style="height: 100%;  padding-bottom: 0%;">
+        <table class="tabela">
+            <thead class="cabecalho">
+                <tr  style="background: #295C8A;">
                     <th class="table_first_row">NOME</th>
                     <th class="table_first_row">UNIDADE DE MEDIDA</th>
                     <th class="table_first_row">VALOR</th>
@@ -31,12 +52,12 @@
             </thead>
             <tbody>
                 <tr>
-                    <td class="table_rows">{{ $material->nome}}</td>
+                    <td class="table_rows">{{ $material->nome }}</td>
                     <td class="table_rows">{{ $material->unidade_medida->unidade }}</td>
                     <td class="table_rows">{{ $material->valor }}</td>
                     <td class="table_rows">{{ $material->codigo }}</td>
-                    <td>{{$material->created_at->format('d/m/Y')}} as {{$material->created_at->format('H:i')}}</td>
-                    <td>{{$material->updated_at->format('d/m/Y')}} as {{$material->updated_at->format('H:i')}}</td>
+                    <td>{{ $material->created_at->format('d/m/Y') }} as {{ $material->created_at->format('H:i') }}</td>
+                    <td>{{ $material->updated_at->format('d/m/Y') }} as {{ $material->updated_at->format('H:i') }}</td>
                 </tr>
             </tbody>
         </table>
