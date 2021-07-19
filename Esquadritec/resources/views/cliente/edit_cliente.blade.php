bd01441b9b2ba74400333c37a506a69a3d4572d8
Joedson <joedsoncte@gmail.com>
2021-07-19 11:31:54 -0300

    Ícone para add mais telefone e endereço ao client

--- Esquadritec/resources/views/cliente/edit_cliente.blade.php
+++ Esquadritec/resources/views/cliente/edit_cliente.blade.php
@@ -22,7 +22,7 @@
             <form class="" style="height: 100%;" method="POST" action="{{route('edite_cliente_do',['id'=>$cliente->id])}}">
                 @csrf
                 <input type="hidden" value="{{$cliente->id}}" name="id">
-                <div class="full-width pr-5 right mb-2">
+                <div class="full-width pr-5 right mb-2  mt-4">
                     <label for="name">Nome:</label>
                     <input class="input-3" type="text" value="{{$cliente->name}}" name="name" required>
                 </div>
@@ -42,12 +42,22 @@
                     <input class="input-3" type="text" value="{{$cliente->email}}" name="email" required>
                 </div>
 
-                <div class="full-width mb-2 right pr-5">
+                <div class="full-width mb-2 right pr-3"  style="width: 99%;">
                     <label for="tel">Telefone:</label>
-                    <input class="input-3" type="text" value="{{$telefone->numero}}" name="telefone" required>
+                    <input class="input-3" type="text" style="width: 43%;" value="{{$telefone->numero}}" name="telefone" required>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill ml-2" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                    </svg>
                 </div>
 
-                <h5 class="mt-3">Endereço</h5>
+                <div class="row mt-3">
+                    <div class="col-7 right"><h5 class="space">Endereço</h5></div>
+                    <div class="col-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                        </svg>
+                    </div>
+                </div>
 
                 <div class="full-width mb-2 right pr-5">
                     <label for="city">Cidade:</label>
@@ -74,7 +84,7 @@
                         style="height: 100px; width: 65%;">{{$endereco->observacao}}</textarea>
                 </div>
 
-                <div class="pt-4">
+                <div class="center">
                     <a href="{{route('dashboard')}}" class="rounded-pill btn btn-sm btn-danger mr-5" type="reset">Cancelar</a>
                     <button class="rounded-pill btn btn-sm btn-success ml-5" type="submit">Confirmar</button>
                 </div>
