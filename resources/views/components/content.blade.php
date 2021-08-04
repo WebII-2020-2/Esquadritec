0dee7301099c78f35aeaea9c682c3dc04e979e11
djalma neto <djalma7743@gmail.com>
2021-08-03 22:34:58 -0300

    debug

--- resources/views/components/content.blade.php
+++ resources/views/components/content.blade.php
@@ -1,3 +1,4 @@
-<link rel="stylesheet" href="{{ asset('site/style.css') }}" crossorigin="anonymous">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+<link rel="stylesheet" href="{{ asset('site/style.css') }}">
 <link rel="stylesheet" href="{{ asset('site/cssPerson.css') }}">
 <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
